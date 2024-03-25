2bfadb591e1d897afdfdc51e6ae2aa164a28c017
srinivaas <snarajava@gmail.com>
2024-03-25 18:49:13 +0530

    creating the Project

---
+++ ArrayListTest.java
@@ -0,0 +1,23 @@
+package com.hdfc.ergo.businees;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+	
+	public static void main(String[] args) {
+		
+		// variables or methods static instance we can call
+		// store retrive manipulate the Data
+		ArrayList<String> listOfCustomers=new ArrayList<>();
+		listOfCustomers.add("Gopi");
+		listOfCustomers.add("Anudeep");
+		// store retrive manipulate the Data
+		ArrayList<Integer> listOfCustomers1=new ArrayList<>();
+		
+		
+		
+			System.out.println(listOfCustomers1.size());	
+		
+	}
+
+}
